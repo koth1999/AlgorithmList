dec2d52f7dcf6dcf44283f0c224594ab2399328b
koth1999 <83741577+koth1999@users.noreply.github.com>
2023-08-19 12:59:12 +0900

    2023/08/19 커밋
    2023/08/19 커밋

---
+++ 문제/숫자의합/SumOfNumbers.java
@@ -0,0 +1,27 @@
+package 문제.숫자의합;
+
+// N개의 숫자가 공백 없이 쓰여있다. 이 숫자를 모두 합해서 출력하는 프로그램을 작성하시오.
+
+// 입력 : 5
+//       5 4 3 2 1
+// 출력 : 15
+
+import java.util.Scanner;
+
+public class SumOfNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String num = "";
+
+        while(true) {
+            int sum = 0;
+            num = sc.next();
+            if(num.equals("0")) break;
+            for(int i = 0; i<num.length(); i++) {
+                sum += num.charAt(i)-'0';
+            }
+            System.out.println(sum);
+        }
+    }
+}
---
+++ 문제/진수변환/OctalBinary.java
@@ -0,0 +1,29 @@
+package 문제.진수변환;
+
+// 8진수가 주어졌을 때, 2진수로 변환하는 프로그램을 작성하시오.
+// 입력 : 314
+// 출력 : 11001100
+
+import java.util.Scanner;
+
+public class OctalBinary {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] bin1 = {"000", "001", "010", "011", "100", "101", "110", "111"};
+        String[] bin2 = {"", "1", "10", "11", "100", "101", "110", "111"};
+        String oct = sc.next();
+
+        for(int i = 0; i<oct.length(); i++) {
+            if(oct.length()==1 && oct.charAt(i)-'0' == 0) {
+                System.out.print("0");
+                break;
+            }
+            if(i==0) {
+                System.out.print(bin2[oct.charAt(0)-'0']);
+            }else {
+                System.out.print(bin1[oct.charAt(i)-'0']);
+            }
+        }
+
+    }
+}
