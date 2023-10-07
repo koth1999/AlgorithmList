4c6783b63b64eebbae9aa14964ad538518ff2aa9
koth1999 <83741577+koth1999@users.noreply.github.com>
2023-10-08 00:08:48 +0900

    2023/10/08 커밋
    2023/10/08 커밋

---
+++ 문제/DivideTheCash/DivideTheCash.java
@@ -0,0 +1,52 @@
+package 문제.DivideTheCash;
+
+// The UCF Programming Team coaches schedule practices regularly in fall and spring (by the way, all UCF students are welcome to the practices).
+// During summer, the majority of the team members are gone but the coaches know how to make sure the students don’t get “rusty”.
+// The coaches usually give prize money and reward the team members based on how many problems they solve during summer.
+// For example, let’s assume the coaches have $1,000 in prize money and there are three students.
+// Let’s also assume the three students solve, respectively, 5, 8 and 7 problems, i.e., a total of 20 problems.
+// So, the reward for one problem will be $50 ($1000/20) and the three team members will receive, respectively, $250, $400 and $350.
+//Given the total prize money and the number of problems each team member has solved, compute the reward for each student.
+
+// 입력
+// The first input line contains two integers: n (1 ≤ n ≤ 30), indicating the number of team members and d (1 ≤ d ≤ 30,000), indicating the dollar amount to be divided among the students.
+// Each of the next n input lines contains an integer (between 1 and 300, inclusive) indicating the number of problems a team member has solved.
+// Assume that the input values are such that the reward for one problem will be an integer number of dollars.
+
+// 출력
+// Print the pay, in dollars, for each team member (in the same order as they appear in the input).
+
+// 입력
+// 3 1000
+// 5 8 7
+
+// 출력
+// 250
+// 400
+// 350
+
+import java.util.Scanner;
+
+public class DivideTheCash {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int d = sc.nextInt(); // 달러
+
+        int[] problems = new int[n];
+
+        for(int i = 0; i < problems.length; i++) {
+            problems[i] = sc.nextInt(); // 문제의 수
+        }
+
+        int totalProblems = 0;
+        for(int i = 0; i < problems.length; i++) {
+            totalProblems += problems[i];
+        }
+
+        for(int i = 0; i < problems.length; i++) {
+            int reward = (problems[i] * d) / totalProblems; // 팀원에 대한 급여
+            System.out.println(reward);
+        }
+    }
+}
---
+++ 문제/HOMWRK/HomeWork.java
@@ -0,0 +1,51 @@
+package 문제.HOMWRK;
+
+// In one of the beautiful cities of Afghanistan two sisters are going to program a simple game to help them solve their mathematics homework.
+// Their homework asks them to calculate the sum and multiplication of two numbers.
+// Your task is to help them to build a simple program for their homework.
+// 입력
+// First line contains the number of test cases (T).
+// T test cases follow. For each test case, the first line represents the number of problems.
+// Next N lines contains two integer numbers Ai, Bi.
+
+// 출력
+// For every input expecting two integer numbers,
+// first one represents the addition of two given numbers and second represents the multiplication separated by space.
+
+// 제한
+// 1 ≤ T ≤ 10
+// 1 ≤ N ≤ 50
+// 0 ≤ Ai, Bi ≤ 20000
+
+// 입력
+// 1
+// 2
+// 20 30
+// 40 60
+
+// 출력
+// 50 600
+// 100 2400
+
+import java.util.Scanner;
+
+public class HomeWork {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        for(int i = 0; i < t; i++) {
+            int n = sc.nextInt();
+
+            for(int j = 0; j < n; j++) {
+                int a = sc.nextInt();
+                int b = sc.nextInt();
+
+                int sum = a + b;
+                int mul = a * b;
+
+                System.out.println(sum + " " + mul);
+            }
+        }
+    }
+}
